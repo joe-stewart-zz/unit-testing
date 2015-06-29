f6ccf8cc5826d2437bfc7ca5232a33c4daa71276
Joe Stewart <joe.stewart.dev@gmail.com>
2015-06-29 16:52:00 -0700

    rewriting Profile and ProfileTest per TDD chapter

--- Profile.java
+++ Profile.java
@@ -1,28 +1,5 @@
-import java.util.*;
-import java.util.stream.*;
-import java.util.function.*;
-
 public class Profile {
-    private AnswerCollection answers = new AnswerCollection();
-    private String name;
-
-    public Profile(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void add(Answer answer) {
-        answers.add(answer);
-    }
-
-    public MatchSet getMatchSet(Criteria criteria) {
-        return new MatchSet(answers, criteria);
-    }
-
-    public String toString() {
-        return name;
+    public boolean matches(Criterion criterion) {
+        return false;
     }
 }
---
+++ ProfileTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ProfileTest {
+    @Test
+    public void matchesNothingWhenProfileEmpty() {
+        Profile profile = new Profile();
+        Question question = new BooleanQuestion(1, "Relocation package?");
+        Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare);
+
+        boolean result = profile.matches(criterion);
+
+        assertFalse(result);
+    }
+}
