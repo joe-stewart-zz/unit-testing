8c55fce94d24789645c60665ba65408eece87f77
Joe Stewart <joe.stewart.dev@gmail.com>
2015-06-30 14:27:04 -0700

    TDD second pass on Profile and ProfileTest

--- Profile.java
+++ Profile.java
@@ -1,5 +1,10 @@
 public class Profile {
+    private Answer answer;
+
     public boolean matches(Criterion criterion) {
-        return false;
+        return answer != null;
+    }
+    public void add(Answer answer) {
+        this.answer = answer;
     }
 }
--- ProfileTest.java
+++ ProfileTest.java
@@ -2,14 +2,35 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion questionIsThereRelocation;
+    private Answer answerThereIsRelocation;
+
+    @Before
+    public void createProfile() {
+        profile = new Profile();
+    }
+    @Before
+    public void createQuestionAndAnswer() {
+        questionIsThereRelocation = new BooleanQuestion(1, "Relocation package?");
+        answerThereIsRelocation = new Answer(questionIsThereRelocation, Bool.TRUE);
+    }
     @Test
     public void matchesNothingWhenProfileEmpty() {
-        Profile profile = new Profile();
-        Question question = new BooleanQuestion(1, "Relocation package?");
-        Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare);
+        Criterion criterion = new Criterion(answerThereIsRelocation, Weight.DontCare);
 
         boolean result = profile.matches(criterion);
 
         assertFalse(result);
+    }
+    @Test
+    public void matchesWhenProfileContainsMatchingAnswer() {
+        Answer answer = new Answer(questionIsThereRelocation, Bool.TRUE);
+        profile.add(answer);
+        Criterion criterion = new Criterion(answerThereIsRelocation, Weight.Important);
+
+        boolean result = profile.matches(criterion);
+
+        assertTrue(result);
     }
 }
